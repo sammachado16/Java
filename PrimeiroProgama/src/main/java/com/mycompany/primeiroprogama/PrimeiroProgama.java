7f938de51e7eba59c0e58bdb256277766b37f58c
Samuel Machado <sam.machado16@gmail.com>
2026-04-27 13:04:18 -0300

    olá mundo

---
+++ PrimeiroProgama/src/main/java/com/mycompany/primeiroprogama/PrimeiroProgama.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprogama;
+
+/**
+ *
+ * @author samma
+ */
+public class PrimeiroProgama {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
